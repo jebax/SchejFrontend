610eeec56757a6001462379461f21485a5d52366
jebax <joel.eliot.baxter@gmail.com>
2018-12-10 14:40:46 +0000

    Set up project directory and dependencies
    Includes Cypress, ESLint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello world!</p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -21,8 +19,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
