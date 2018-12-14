f4059ff6d0189ac345807e6deb5597bdcecc3995
Anouska Hopkins <anouskahopkins@gmail.com>
2018-12-14 12:15:54 +0000

    Test driven drop down for selecting shifts to swap

--- cypress/integration/request_shift_swap.spec.js
+++ cypress/integration/request_shift_swap.spec.js
@@ -2,7 +2,8 @@ describe('Requesting a shift swap', () => {
   before(() => {
     const data = {
       name: 'TestName',
-      organisation: 'Testorganisation'
+      organisation: 'Testorganisation',
+      id: '1'
     }
 
     cy.server()
@@ -25,6 +26,23 @@ describe('Requesting a shift swap', () => {
           start_time: '1544601600000',
           end_time: '1544616000000'
         }
+      }
+    })
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3001/api/v1/shiftsbyuser/1',
+      response: {
+        data: [
+          {
+            id: "1",
+            title: "TestName",
+            start_time: '1544601600000',
+            end_time: '1544616000000',
+            user_id: "1",
+            organisation: "Testorganisation",
+            email: "TestEmail"
+          }
+        ]
       }
     })
     cy.visit('http://localhost:3000/sign_up')
@@ -69,5 +87,11 @@ describe('Requesting a shift swap', () => {
   it('can render a shift swapping form', () => {
     cy.get('[class="custom-button"]').click()
     cy.contains('Request a shift swap')
+  })
+
+  it.skip('can select own shift', () => {
+    cy.get('[id="user-shifts-list"]').click()
+    cy.get('[className="menu-item"]').click()
+    cy.get('[id="chosen-shift"]').should('have.value', '1')
   })
 })
