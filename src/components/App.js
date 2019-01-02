1f12ca0304cde4ec57700c0b4583335b3c64ad76
jebax <joel.eliot.baxter@gmail.com>
2019-01-02 23:56:39 +0000

    Changed App and IndexTitle to functional components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Main from './Main'
 
-export default class App extends Component {
-
-  render () {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous" />
-        </header>
-        <Main />
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous" />
+      </header>
+      <Main />
+    </div>
+  )
 }
+
+export default App
--- src/components/IndexTitle.js
+++ src/components/IndexTitle.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class IndexTitle extends Component {
-  render() {
-    return (
-      <section id='index-title'>
-        <h1 id='index-title-text'>Schej</h1>
-      </section>
-    )
-  }
+const IndexTitle = () => {
+  return (
+    <section
+      id='index-title'
+    >
+      <h1
+        id='index-title-text'
+      >
+        Schej
+      </h1>
+    </section>
+  )
 }
+
+export default IndexTitle
